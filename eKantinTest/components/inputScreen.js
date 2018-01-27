87746dd21a27fffbf156e5c8b3b7c5d0c164a64c
AhmadNizar <ahmadnizar.owl@gmail.com>
2018-01-27 17:04:07 +0700

    -add form to 1st page

--- eKantinTest/components/inputScreen.js
+++ eKantinTest/components/inputScreen.js
@@ -1,14 +1,32 @@
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
+import { FormLabel, FormInput, Button } from 'react-native-elements'
 
 export default class InputScreen extends Component {
   constructor(props) {
     super(props)
+    this.state = {
+      inputData: ''
+    }
+  }
+  onChangeMethod(e) {
+    this.setState({
+      inputData: e
+    })
+  }
+  onSubmitMethod() {
+    console.log('jos')
   }
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>InputScreen Screen jos</Text>
+        <FormLabel>Input Data</FormLabel>
+        <FormInput placeholder = 'Your Input Data' onChangeText={(e) => this.onChangeMethod(e)} />
+        <Button
+          raised
+          title='Submit'
+          onPress={() => this.onSubmitMethod()}
+        />
       </View>
     )
   }
